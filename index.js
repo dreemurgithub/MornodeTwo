1c7be57c773881db46b9aa71f9897eda167eea51
Dat <lethanhdat1993@gmail.com>
2023-09-21 17:27:43 +0700

    update some docker stuffs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('one.db');
-
+db.exec('CREATE TABLE users (  id INTEGER PRIMARY KEY AUTOINCREMENT,    name TEXT,    email TEXT );')
+// everytime we rebuild the docker we will lose the sqlite3 data
 const app = express()
 const port = 3000
 
